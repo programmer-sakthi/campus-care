6dc567dfdf37f2313f3ba617e1e8a9b9c71ca0c5
sakthi <sakthipalanisamy7@gmail.com>
2026-08-01 19:36:50 +0530

    exported prisma client

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,10 +3,10 @@ import express from "express";
 const app = express();
 
 
-app.listen(8000, () => {
-  console.log("Booking Service is running on port 8000");
+app.listen(process.env.PORT, () => {
+  console.log(`Backend is running on port ${process.env.PORT}`);
 });
-
+  
 app.get("/", (req, res) => {
   res.send("Booking Service is running!");
 });
